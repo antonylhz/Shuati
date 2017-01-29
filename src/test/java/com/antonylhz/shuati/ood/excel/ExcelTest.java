9f5693923fc20f7dc589173537d13ac8a01d90bf
Hongzhang Liu <hongzhal@amazon.com>
2017-01-29 03:12:31 -0500

    Removed leetcode downloads from Gradle compile directory

--- src/test/java/com/antonylhz/shuati/ood/excel/ExcelTest.java
+++ src/test/java/com/antonylhz/shuati/ood/excel/ExcelTest.java
@@ -9,13 +9,6 @@ import org.junit.Test;
 public class ExcelTest {
 
     @Test
-    public void testGetNumericalId() throws Exception {
-        Assert.assertEquals(27 , Excel.getNumericalId("AA"));
-        Assert.assertEquals(676 , Excel.getNumericalId("YZ"));
-
-    }
-
-    @Test
     public void testGetAlphabeticalId() throws Exception {
         Assert.assertEquals("AA" , Excel.getAlphabeticalColumn(27));
         Assert.assertEquals("YZ" , Excel.getAlphabeticalColumn(676));
